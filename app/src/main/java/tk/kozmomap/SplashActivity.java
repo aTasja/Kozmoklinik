4ab5621249b7cc460b3fb9b029d96d9bf9f90558
Anastasiya Shatova <a_tasja@mail.ru>
2019-06-10 14:48:34 +0300

    june 2019 update

--- app/src/main/java/tk/kozmomap/SplashActivity.java
+++ app/src/main/java/tk/kozmomap/SplashActivity.java
@@ -1,8 +1,12 @@
 package tk.kozmomap;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -26,6 +30,15 @@ public class SplashActivity extends AppCompatActivity {
         mp = MediaPlayer.create(SplashActivity.this, R.raw.splash_sound);
         mp.start();
 
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.INTERNET
+            }, 10);
+        }
+
         new Handler().postDelayed (new Runnable() {
             @Override
             public void run() {
@@ -33,6 +46,7 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
 
                 finish ();
+
             }
         }, SPLASH_TIME_OUT);
     }
@@ -43,4 +57,19 @@ public class SplashActivity extends AppCompatActivity {
         mp.stop();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                }
+
+            }
+        }
+    }
 }
